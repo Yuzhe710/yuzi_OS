05a0b7a3bf7c80018873253dd328ff40c7dbcef3
Yuzhe710 <jackie2021ars@gmail.com>
2023-01-03 16:23:44 +0800

    printf - update display

--- kernel.cpp
+++ kernel.cpp
@@ -4,10 +4,39 @@
 
 void printf(char* str)
 {
-    uint16_t* VideoMemory = (uint16_t*)0xb8000;
+    static uint16_t* VideoMemory = (uint16_t*)0xb8000;
+
+    static uint8_t x = 0, y = 0;
 
     for (int i = 0; str[i] != '\0'; ++i)
-        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i]; // preserve the high bytes
+    {
+        switch(str[i])
+        {
+            case '\n': // input a linefeed
+                x = 0;
+                y++;
+                break;
+            default:
+                VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | str[i];  // preserve the high bytes
+                x ++;
+                break;
+        }
+
+        if (x >= 80) // finish a line 
+        {
+            x = 0;
+            y++;
+        }
+
+        if (y >= 25) // finish the whole screen
+        {
+            for (y = 0; y < 25; y++)
+                for (x = 0; x < 80; x++)
+                    VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | ' ';
+            x = 0;
+            y = 0;
+        }
+    }
 }
 
 typedef void (*constructor)();       // define constructor as the function pointer
@@ -23,6 +52,7 @@ extern "C" void callConstructors()
 
 extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnuumber)
 {
+    printf("Hello World!\n");
     printf("Hello World!");
     
     while (1);
