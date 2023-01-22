57a13e22e9e7d97c9ab66e4380c1de5b6061dfd7
Yuzhe710 <jackie2021ars@gmail.com>
2023-01-22 22:16:25 +1030

    Update keyboard.cpp

--- keyboard.cpp
+++ keyboard.cpp
@@ -15,3 +15,22 @@ commandport(0x64)
 
     dataport.Write(0xF4);
 }
+
+KeyboardDriver::~KeyboardDriver()
+{
+}
+
+void printf(char*);
+
+uint32_t KeyboardDriver::HandleInterrupt(uint32_t esp)
+{
+    uint8_t key = dataport.Read();
+
+    char* foo = "KEYBOARD 0x00 ";
+    char* hex = "0123456789ABCDEF";
+    foo[11] = hex[(key >> 4) & 0x0F];
+    foo[12] = hex[key & 0x0F];
+    printf(foo);
+
+    return esp;
+}
