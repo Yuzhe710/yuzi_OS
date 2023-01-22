d145ba3070a9f203966d99c2500ecdb49b06a859
Yuzhe710 <jackie2021ars@gmail.com>
2023-01-22 22:23:46 +1030

    Update keyboard.cpp

--- keyboard.cpp
+++ keyboard.cpp
@@ -28,8 +28,8 @@ uint32_t KeyboardDriver::HandleInterrupt(uint32_t esp)
 
     char* foo = "KEYBOARD 0x00 ";
     char* hex = "0123456789ABCDEF";
-    foo[11] = hex[(key >> 4) & 0x0F];
-    foo[12] = hex[key & 0x0F];
+    foo[11] = hex[(key >> 4) & 0xF];
+    foo[12] = hex[key & 0xF];
     printf(foo);
 
     return esp;
